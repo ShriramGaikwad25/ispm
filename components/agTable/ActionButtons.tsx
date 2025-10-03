5860209eced9f5a5538512a3f1eca7573e896cc5
ShriramGaikwad25 <gaikwad.shriram@keyforge.ai>
2025-10-03 23:30:56 +0530

    change done

--- components/agTable/ActionButtons.tsx
+++ components/agTable/ActionButtons.tsx
@@ -51,7 +51,7 @@ const ActionButtons = <T extends { status?: string }>({
   certId,
   viewChangeEnable,
   onActionSuccess,
-}: ActionButtonsProps<T>) => {
+}: ActionButtonsProps<T>): JSX.Element => {
   const { queueAction } = useActionPanel();
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const menuButtonRef = useRef<HTMLButtonElement>(null);
